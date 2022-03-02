7128766948e0a6e0533120d3df0298da9544b7eb
Maximilian Klötzer <maximilian.kloetzer@gmail.com>
2022-03-02 13:09:02 +0100

    Rename tabpages

--- src/FoosballScoreboard/Main.Designer.cs
+++ src/FoosballScoreboard/Main.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.TpTournament = new System.Windows.Forms.TabPage();
+            this.BtnApplyTournament = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtTournamentName = new System.Windows.Forms.TextBox();
             this.TpScore = new System.Windows.Forms.TabPage();
             this.GroupBoxBlack = new System.Windows.Forms.GroupBox();
             this.TxtBlackTeamName2 = new System.Windows.Forms.TextBox();
@@ -53,20 +59,14 @@
             this.TxtGreenSets = new System.Windows.Forms.TextBox();
             this.BtnGreenSetUp = new System.Windows.Forms.Button();
             this.BtnResetScore = new System.Windows.Forms.Button();
-            this.TpTournament = new System.Windows.Forms.TabPage();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.OpenSavedPath = new System.Windows.Forms.ToolStripMenuItem();
-            this.TxtTournamentName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.BtnApplyTournament = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.TpTournament.SuspendLayout();
             this.TpScore.SuspendLayout();
             this.GroupBoxBlack.SuspendLayout();
             this.GroupBoxGreen.SuspendLayout();
-            this.TpTournament.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -80,6 +80,74 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1003, 685);
             this.tabControl1.TabIndex = 0;
+            // 
+            // TpTournament
+            // 
+            this.TpTournament.Controls.Add(this.BtnApplyTournament);
+            this.TpTournament.Controls.Add(this.label2);
+            this.TpTournament.Controls.Add(this.textBox1);
+            this.TpTournament.Controls.Add(this.label1);
+            this.TpTournament.Controls.Add(this.TxtTournamentName);
+            this.TpTournament.Location = new System.Drawing.Point(4, 34);
+            this.TpTournament.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TpTournament.Name = "TpTournament";
+            this.TpTournament.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TpTournament.Size = new System.Drawing.Size(995, 647);
+            this.TpTournament.TabIndex = 1;
+            this.TpTournament.Text = "Tournament";
+            this.TpTournament.UseVisualStyleBackColor = true;
+            // 
+            // BtnApplyTournament
+            // 
+            this.BtnApplyTournament.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.BtnApplyTournament.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnApplyTournament.Location = new System.Drawing.Point(778, 584);
+            this.BtnApplyTournament.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnApplyTournament.Name = "BtnApplyTournament";
+            this.BtnApplyTournament.Size = new System.Drawing.Size(209, 53);
+            this.BtnApplyTournament.TabIndex = 23;
+            this.BtnApplyTournament.Text = "Apply";
+            this.BtnApplyTournament.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(34, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 41);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Competition";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(34, 200);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(517, 45);
+            this.textBox1.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(34, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 41);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Tournament Name";
+            // 
+            // TxtTournamentName
+            // 
+            this.TxtTournamentName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtTournamentName.Location = new System.Drawing.Point(34, 80);
+            this.TxtTournamentName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtTournamentName.Name = "TxtTournamentName";
+            this.TxtTournamentName.Size = new System.Drawing.Size(952, 45);
+            this.TxtTournamentName.TabIndex = 1;
             // 
             // TpScore
             // 
@@ -368,22 +436,6 @@
             this.BtnResetScore.UseVisualStyleBackColor = true;
             this.BtnResetScore.Click += new System.EventHandler(this.BtnResetScore_Click);
             // 
-            // TpTournament
-            // 
-            this.TpTournament.Controls.Add(this.BtnApplyTournament);
-            this.TpTournament.Controls.Add(this.label2);
-            this.TpTournament.Controls.Add(this.textBox1);
-            this.TpTournament.Controls.Add(this.label1);
-            this.TpTournament.Controls.Add(this.TxtTournamentName);
-            this.TpTournament.Location = new System.Drawing.Point(4, 34);
-            this.TpTournament.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TpTournament.Name = "TpTournament";
-            this.TpTournament.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TpTournament.Size = new System.Drawing.Size(995, 647);
-            this.TpTournament.TabIndex = 1;
-            this.TpTournament.Text = "Tournament";
-            this.TpTournament.UseVisualStyleBackColor = true;
-            // 
             // menuStrip1
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
@@ -411,58 +463,6 @@
             this.OpenSavedPath.Text = "Speicherort öffnen";
             this.OpenSavedPath.Click += new System.EventHandler(this.OpenSavedPath_Click);
             // 
-            // TxtTournamentName
-            // 
-            this.TxtTournamentName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.TxtTournamentName.Location = new System.Drawing.Point(34, 80);
-            this.TxtTournamentName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TxtTournamentName.Name = "TxtTournamentName";
-            this.TxtTournamentName.Size = new System.Drawing.Size(952, 45);
-            this.TxtTournamentName.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(34, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(264, 41);
-            this.label1.TabIndex = 20;
-            this.label1.Text = "Tournament Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(34, 154);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(197, 41);
-            this.label2.TabIndex = 22;
-            this.label2.Text = "Competitions";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(34, 200);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(517, 45);
-            this.textBox1.TabIndex = 21;
-            // 
-            // BtnApplyTournament
-            // 
-            this.BtnApplyTournament.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.BtnApplyTournament.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnApplyTournament.Location = new System.Drawing.Point(778, 584);
-            this.BtnApplyTournament.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.BtnApplyTournament.Name = "BtnApplyTournament";
-            this.BtnApplyTournament.Size = new System.Drawing.Size(209, 53);
-            this.BtnApplyTournament.TabIndex = 23;
-            this.BtnApplyTournament.Text = "Apply";
-            this.BtnApplyTournament.UseVisualStyleBackColor = true;
-            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
@@ -476,13 +476,13 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Main_Load);
             this.tabControl1.ResumeLayout(false);
+            this.TpTournament.ResumeLayout(false);
+            this.TpTournament.PerformLayout();
             this.TpScore.ResumeLayout(false);
             this.GroupBoxBlack.ResumeLayout(false);
             this.GroupBoxBlack.PerformLayout();
             this.GroupBoxGreen.ResumeLayout(false);
             this.GroupBoxGreen.PerformLayout();
-            this.TpTournament.ResumeLayout(false);
-            this.TpTournament.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
