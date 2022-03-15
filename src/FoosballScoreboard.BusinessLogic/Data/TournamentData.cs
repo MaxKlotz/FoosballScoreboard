d52bc765aa3e8a37d4a34b2f12fa8e91dd94a80d
Maximilian Klötzer <maximilian.kloetzer@gmail.com>
2022-03-15 22:23:57 +0100

    Add class for tournamentdata

---
+++ src/FoosballScoreboard.BusinessLogic/Data/TournamentData.cs
@@ -0,0 +1,19 @@
+﻿using FoosballScoreboard.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoosballScoreboard.BusinessLogic.Data
+{
+    public class TournamentData: INotifyPropertyChangedWithChangeSet
+    {
+        private string _tournamentName = string.Empty;
+        private string _competition = string.Empty;
+
+        public event INotifyPropertyChangedWithChangeSet.PropertyChangedWithChangesetEventHandler? PropertyChangedWithChangeset;
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
